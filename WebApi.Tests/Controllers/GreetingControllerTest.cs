d25bc2ce2d482d141d0868460fcfccf251af95da
YonlinZ <zylin0721@foxmail.com>
2020-03-01 23:04:45 +0800

    .

---
+++ WebApi.Tests/Controllers/GreetingControllerTest.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WebApi.Controllers;
+using WebApi.Models;
+
+namespace WebApi.Tests.Controllers
+{
+    [TestClass]
+    public class GreetingControllerTest
+    {
+        [TestMethod]
+        public void PostGreeting()
+        {
+            //arrange
+            var name = "greetingName";
+            var message = "greetingMessage";
+            var greeting = new Greeting(){Name = name, Message = message};
+            var fakeRequest = new HttpRequestMessage(HttpMethod.Post, new Uri("http://localhost:5581/api/greeting"));
+            var service = new GreetingController();
+            service.Request = fakeRequest;
+            // act
+            var response = service.PostGreeting(greeting);
+            //assert
+            Assert.IsNotNull(response);
+            Assert.AreEqual(HttpStatusCode.Created, response.StatusCode);
+            Assert.AreEqual(new Uri("http://localhost:5581/api/greeting/greetingName"), response.Headers.Location);
+
+
+        }
+    }
+}
---
+++ WebApiHost/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Windows.Forms;
+using System.Web.Http.SelfHost;
+
+namespace WebApiHost
+{
+    static class Program
+    {
+        /// <summary>
+        /// 应用程序的主入口点。
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Assembly.Load("WebApi");
+            var config = new HttpSelfHostConfiguration(new Uri("http://localhost:8848"));
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+            var host = new HttpSelfHostServer(config);
+            host.OpenAsync().Wait();
+
+            //Console.WriteLine("HttpSelfHostServer服务已启动，如果需要退出，请按任意键。");
+            //Console.ReadKey();
+            Application.Run(new Form1());
+            host.CloseAsync().Wait();
+        }
+    }
+}
